64990fa3cc8771feb3acb43d4ce5acdcffbd3458
LorenzoF <lorenzoferritsic@gmail.com>
2022-08-21 13:42:18 -0400

    Adjusted html, adding text areas for user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,58 @@
-var daysList = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
 
-$(document).ready(function() {
+function loadDate() {
+    var currentDay = document.getElementById("currentDay")
+    var daysList = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     var day = moment().day();
-    $("#currentDay").text(daysList[day] + ", " + moment().format("MMMM Do"))
+    currentDay.textContent = daysList[day] + ", " + moment().format("MMMM Do")
+}
+
+const myElement = document.getElementById('timeBlockContainer');
+for (let i = 0; i < myElement.children.length; i++) {
+  console.log(myElement.children[i].tagName);
+}
+
+
+function timeBlockCheck() {
+    for (var i = 0; i < 9; i++) {
+    var currentHour = moment().format("HH");
+    var currentTimeBlock = document.getElementById("timeBlockContainer")
+    var children = currentTimeBlock.children;
+    for (var i = 0; i < children.length; i++) {
+        var child = children[i]
+        if (child.time < currentHour) {
+            console.log("early")
+        }
+    }
+    
+    }
+}
+
+// function timeBlockReference() {
+//     for (var i = 0; i < 9; i++) {
+//         var currentHour = moment().format("HH");
+//         var currentTimeblock = $("#timeBlockContainer > section");
+//         // console.log(currentTimeblock)
+//         var attrValue = currentTimeblock.dataset.value;
+//         // console.log(attrValue)
+//         // console.log(currentTimeblock)
+//         if (currentTimeblock.attr("data-time") < currentHour) {
+            
+//             currentTimeblock.p.addClass("past");
+//             currentTimeblock.p.removeClass("present");
+//             currentTimeblock.p.removeClass("future");
+//         }
+//         console.log(currentHour);
+
+
+//     }
+
+// }
+
+// timeBlockReference()
+
+window.addEventListener("load", function() {
+    loadDate()
+    timeBlockCheck()
 })
 
